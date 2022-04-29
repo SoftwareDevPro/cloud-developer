1f50b1fb604590ee6eb7ad9fafaafa1f8f23a3a6
Christopher Adamson <software.pro.08@gmail.com>
2022-04-29 08:32:04 -0700

    add of business (todos) and data layer (todosAcess) changes

--- course-04/project/c4-final-project-starter-code/backend/src/helpers/todos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/helpers/todos.ts
@@ -1,10 +1,67 @@
-import { TodosAccess } from './todosAcess'
-import { AttachmentUtils } from './attachmentUtils';
+import { TodoAccess } from './todosAcess'
 import { TodoItem } from '../models/TodoItem'
+import { TodoUpdate } from '../models/TodoUpdate';
 import { CreateTodoRequest } from '../requests/CreateTodoRequest'
 import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
 import { createLogger } from '../utils/logger'
 import * as uuid from 'uuid'
-import * as createError from 'http-errors'
 
 // TODO: Implement businessLogic
+
+const todoAccess = new TodoAccess()
+
+const logger = createLogger('todos')
+
+export async function createTodo(req: CreateTodoRequest, userId: string): Promise<TodoItem> {
+    logger.info("createTodo", { userId: userId, name: req.name, duedate: req.dueDate });
+
+    const createdAt = new Date().toISOString()
+    const todoId = uuid.v4()
+
+    return await todoAccess.createTodo({
+        userId,
+        todoId,
+        createdAt,
+        name: req.name,
+        dueDate: req.dueDate,
+        done: false,
+    })
+}
+
+export async function deleteTodo(userId: string, todoId: string) {
+    logger.info("deleteTodo", { userId: userId, todoId: todoId });
+
+    return await todoAccess.deleteTodo(userId, todoId)
+}
+
+export async function updateTodo(req: UpdateTodoRequest, userId: string, todoId: string): Promise<TodoUpdate> {
+    logger.info("updateTodo", { userId: userId, todoId: todoId, name: req.name, duedate: req.dueDate, done: req.done });
+    
+    return await todoAccess.updateTodo({
+        name: req.name,
+        dueDate: req.dueDate,
+        done: req.done
+    }, todoId, userId)
+}
+
+export async function createAttachmentPresignedUrl(url, userId: string, todoId: string): Promise<TodoItem> {
+    logger.info("createAttachmentPresignedUrl", { userId: userId, todoId: todoId, url: url.attachmentUrl });
+    
+    return await todoAccess.createAttachmentPresignedUrl({
+      userId,
+      todoId,
+      attachmentUrl: url.attachmentUrl,
+    })
+}
+
+export async function getTodo(userId: string, todoId: string): Promise<TodoItem[]> {
+    logger.info("getTodo", { userId: userId, todoId: todoId });
+
+    return todoAccess.getTodo(userId, todoId)
+}
+
+export async function getTodosForUser(userId: string): Promise<TodoItem[]> {
+    logger.info("getTodosForUser", { userId: userId });
+
+    return todoAccess.getTodosForUser(userId)
+}
--- course-04/project/c4-final-project-starter-code/backend/src/helpers/todosAcess.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/helpers/todosAcess.ts
@@ -1,5 +1,5 @@
 import * as AWS from 'aws-sdk'
-import * as AWSXRay from 'aws-xray-sdk'
+const AWSXRay = require('aws-xray-sdk');
 import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 import { createLogger } from '../utils/logger'
 import { TodoItem } from '../models/TodoItem'
@@ -8,5 +8,116 @@ import { TodoUpdate } from '../models/TodoUpdate';
 const XAWS = AWSXRay.captureAWS(AWS)
 
 const logger = createLogger('TodosAccess')
-
+  
 // TODO: Implement the dataLayer logic
+export class TodoAccess {
+
+    constructor(
+      private readonly docClient: DocumentClient = new XAWS.DynamoDB.DocumentClient(),
+      private readonly table = process.env.TODOS_TABLE
+    ){}
+    
+    async createTodo(todo: TodoItem): Promise<TodoItem> {
+      logger.info("createTodo", { todo: JSON.stringify(todo) });
+
+      await this.docClient.put({
+        TableName: this.table,
+        Item: todo,
+      }).promise()
+      
+    return todo   
+  }
+
+  async deleteTodo(userId: string, todoId: string) {
+    logger.info("deleteTodo", { userId: userId, todoId: todoId });
+
+    const params = {
+        TableName: this.table,
+        Key: { todoId, userId }
+    }
+
+    await this.docClient.delete(params, function(err) {
+      if (err) {
+          logger.error("failed to delete item")
+      } else {
+        logger.info("item deleted ")
+      }
+    }).promise()
+  }
+
+  async getTodosForUser(userId: string): Promise<TodoItem[]> {
+      logger.info("getTodosForUser", { userId: userId  });
+  
+      const result = await this.docClient.query({
+        TableName: this.table,
+        KeyConditionExpression: 'userId = :userId',
+        ExpressionAttributeValues: {
+            ':userId': userId
+        }
+      }).promise()
+        
+      const items = result.Items
+  
+      return items as TodoItem[]
+  }
+
+  async updateTodo(update: TodoUpdate, todoId: string, userId: string): Promise<TodoUpdate> {
+      logger.info("updateTodo", { userId: userId, todoId: todoId, done: update.done });
+
+      await this.docClient.update({
+          TableName: this.table,
+          Key: { 
+            todoId: todoId, 
+            userId: userId 
+          },
+          ExpressionAttributeNames: {"#todo_name": "name"},
+          UpdateExpression: "set #todo_name = :name, dueDate = :dueDate, done = :done",
+          ExpressionAttributeValues: {
+              ":name": update.name,
+              ":dueDate": update.dueDate,
+              ":done": update.done,
+          },
+          ReturnValues: "UPDATED_NEW"
+      }).promise()
+        
+      return update   
+  }
+
+  async getTodo(userId: string, todoId: string): Promise<TodoItem[]> {
+      logger.info("getTodo", { userId: userId, todoId: todoId });
+      
+      const result = await this.docClient.query({
+        TableName: this.table,
+        KeyConditionExpression: 'userId = :userId AND todoId = :todoId',
+        ExpressionAttributeValues: {
+            ':userId': userId,
+            ':todoId': todoId
+        }
+      }).promise()
+      
+      const items = result.Items
+  
+      return items as TodoItem[]
+  }
+
+  async createAttachmentPresignedUrl(update: any): Promise<TodoItem> {
+    logger.info("createAttachmentPresignedUrl");
+    
+    await this.docClient.update({
+        TableName: this.table,
+        Key: { 
+          todoId: update.todoId, 
+          userId: update.userId 
+        },
+        ExpressionAttributeNames: {"#aurl": "attachmentUrl"},
+        UpdateExpression: "set #aurl = :attachmentUrl",
+        ExpressionAttributeValues: {
+            ":attachmentUrl": update.attachmentUrl,
+        },
+        ReturnValues: "UPDATED_NEW"
+      }).promise()
+        
+      return update  
+  }      
+
+}
