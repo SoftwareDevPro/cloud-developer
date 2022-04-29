332874b13d0d04f2c13d0e822b38258e36fda0ae
Christopher Adamson <software.pro.08@gmail.com>
2022-04-29 07:56:01 -0700

    add of changes for get todos, and update todo

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
@@ -3,10 +3,12 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
-
-import { updateTodo } from '../../businessLogic/todos'
+import { createLogger } from '../../utils/logger'
+import { getTodo, updateTodo } from '../../helpers/todos'
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 import { getUserId } from '../utils'
+
+const logger = createLogger('updateTodo')
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
@@ -14,9 +16,26 @@ export const handler = middy(
     const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
     // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
 
+    logger.info("processing event", { event: event })
 
-    return undefined
-)
+    const userId = getUserId(event)
+    
+    const item = await getTodo(userId, todoId)
+    if (item.length === 0) {
+      logger.error("invalid todo", { todoId: todoId })
+      return {
+          statusCode: 404,
+          body: 'todo does not exist'
+        }
+    }
+  
+    const items = await updateTodo(updatedTodo, userId, todoId)
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify(items)
+    }
+})
 
 handler
   .use(httpErrorHandler())
@@ -24,4 +43,4 @@ handler
     cors({
       credentials: true
     })
-  )
+)
