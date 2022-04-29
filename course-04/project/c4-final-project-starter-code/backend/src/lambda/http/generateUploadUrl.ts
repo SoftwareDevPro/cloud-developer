2fe37d94498604ff11b6eac2be4288f9e78e2b33
Christopher Adamson <software.pro.08@gmail.com>
2022-04-29 08:00:22 -0700

    add of changes for generate upload url

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
@@ -3,17 +3,38 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
-
-import { createAttachmentPresignedUrl } from '../../businessLogic/todos'
+import { AttachmentUtils } from '../../helpers/attachmentUtils';
+import { createAttachmentPresignedUrl } from '../../helpers/todos'
 import { getUserId } from '../utils'
+import { createLogger } from '../../utils/logger'
+
+const logger = createLogger('generateUploadUrl')
+
+const bucketName = process.env.ATTACHMENT_S3_BUCKET
+
+const attachmentUtils = new AttachmentUtils();
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const todoId = event.pathParameters.todoId
     // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
     
+    logger.info("processing event", { event: event })
 
-    return undefined
+    const updateUrl = attachmentUtils.getUploadUrl(todoId)
+    const userId = getUserId(event)
+    const url = {
+      attachmentUrl: `https://${bucketName}.s3.amazonaws.com/${todoId}`
+    }
+
+    await createAttachmentPresignedUrl(url, userId, todoId)
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        updateUrl
+      })
+    }
   }
 )
 
@@ -23,4 +44,4 @@ handler
     cors({
       credentials: true
     })
-  )
+)
